ede842fa9749eae3e30fe59589a6a7ebe7cada8d
okul1999 <luko@thenewprogramming.com>
2013-11-07 14:04:35 +0100

    did  some basic things

--- Sint.py
+++ Sint.py
@@ -1,1 +1,22 @@
 import pygame
+
+def main(screen, clock):
+    print(screen)
+    print(clock)
+    run = True
+    bike = pygame.image.load("res/bike.png").convert()
+    bike.set_colorkey((255, 255, 255))
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE: 
+                run = False
+        screen.fill((0, 255, 0))
+        screen.blit(bike, (0, 0))
+        pygame.display.update()
+
+if __name__ == "__main__":
+    pygame.init()
+    sc = pygame.display.set_mode((400, 400))
+    pygame.display.set_caption("Fietsen")
+    cl = pygame.time.Clock()
+    main(sc, cl)
